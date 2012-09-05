a0138684e3e38b6471dfd687d47e23ff8541d3ac
Stephen Yakkel <yakkelsj@muohio.edu>
2012-09-05 03:01:56 -0400

    Added some comments

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -36,6 +36,7 @@ class CatPictureApp : public AppBasic {
 	void gradient(uint8_t* pixels, int x, int y);
 	void circle(uint8_t* pixels, int radius, int centerx, int centery, Color8u c);
 	void triangle(uint8_t* pixels, int length, int x, int y, Color8u c);
+
 };
 
 void CatPictureApp::prepareSettings(Settings* settings){
@@ -46,7 +47,6 @@ void CatPictureApp::prepareSettings(Settings* settings){
 
 void CatPictureApp::setup(){
 
-	Surface cat_picture(loadImage( loadResource(RES_SNOWBOARD) ));
 	mySurface_ = new Surface(textureSize,textureSize,false);
 	myTexture_ = new gl::Texture(*mySurface_);
 	uint8_t* pixelArray = (*mySurface_).getData();
@@ -55,16 +55,20 @@ void CatPictureApp::setup(){
 
 void CatPictureApp::circle(uint8_t* pixels, int radius, int centerx, int centery, Color8u c){
 
-	if(radius <= 0)
+	if(radius <= 0) 
 		return;
 
 	for(int y = centery - radius; y <= centery + radius; y++){
+		// starts from top of circle
 		for(int x = centerx - radius; x <= centerx + radius; x++){
+			//starts from left side of circle and draws a horizontal line
 
 			if(y < 0 || x < 0 || x >= appWidth || y >= appHeight) 
 				continue;
 
 			int distance = (int) sqrt(pow((x - centerx), 2.0)  + pow((y - centery), 2.0));
+			// use distance formula to find the distance between the pixel the program is at
+			// and the center pixel of the circle.
 
 			if(distance <= radius){
 				pixels[3 * (x + y * textureSize)] = c.r;
@@ -77,21 +81,23 @@ void CatPictureApp::circle(uint8_t* pixels, int radius, int centerx, int centery
 
 void CatPictureApp::triangle(uint8_t* pixels, int length, int x, int y, Color8u c){
 
-	if((x + length) <= appWidth){
+	if(((x + length) <= appWidth) && ((y + length) <= appHeight)){
 
 		int height = length;
 
 		for(int count1 = 0; count1 <= height; count1++){
+			// starts from bottom of the triangle
 			for(int count2 = 0; count2 <= length; count2++){
+				// starts from the left of the triangle and draws a horizontal line
 
 		        pixels[(3 * x) + (y * textureSize * 3 )] = c.r;
 		        pixels[(3 * x) + (y * textureSize * 3 ) + 1] = c.g;
 		        pixels[(3 * x) + (y * textureSize * 3 ) + 2] = c.b;
 		        x++;
 	        }
-			x = x - length;
-			length = length - 2;
-			y--;
+			x = x - length; // reset x so you start above the last horizontal line
+			length = length - 2; // subtract 1 pixel from each side of the next line
+			y--; // moves to the row of pixels above the last line we drew
 		}
     }
 }
@@ -102,9 +108,13 @@ void CatPictureApp::gradient(uint8_t* pixels, int x, int y){
 	Color8u c = Color8u(0, 0, 0);
 	
 	for(int y = 0; y < textureSize; y++){
+		// starts from the top of the window
 		for(int x = 0; x < appWidth; x++){
+			// starts from the left of the window
 
 			int num = (int)((256 * x) / appWidth);
+			// number that you add to the color so that you don't reset to black or
+			// go past the appWidth
         
 		pixels[(3 * x) + (y * textureSize * 3 )] = c.r;
 		pixels[(3 * x) + (y * textureSize * 3) + 1] = c.g;
@@ -116,11 +126,11 @@ void CatPictureApp::gradient(uint8_t* pixels, int x, int y){
 
 void CatPictureApp::rectangle(uint8_t* pixels, int x1, int x2, int y1, int y2, Color8u c){
 
-
 	int startx = (x1 < x2) ? x1 : x2;
 	int endx = (x1 < x2) ? x2 : x1;
 	int starty = (y1 < y2) ? y1 : y2;
 	int endy = (y1 < y2) ? y2 : y1;
+	// these determine which are the starting and ending points of the rectangle
 
 	if(endx < 0)
 		return; 
@@ -134,7 +144,10 @@ void CatPictureApp::rectangle(uint8_t* pixels, int x1, int x2, int y1, int y2, C
 	if(endy >= appHeight) endy = appHeight - 1;
 
 	for(int y = starty; y < endy; y++){
+		// starts from top
 		for(int x = startx; x < endx; x++){
+			// starts from the left and draws a horizontal line
+
 		pixels[(3 * x) + (y * textureSize * 3 )] = c.r;
 		pixels[(3 * x) + (y * textureSize * 3) + 1] = c.g;
 		pixels[(3 * x) + (y * textureSize * 3) + 2] = c.b;
@@ -149,14 +162,14 @@ void CatPictureApp::update(){
 
 	uint8_t* pixelArray = (*mySurface_).getData();
 	gradient(pixelArray, 0, 0);
-	triangle(pixelArray, 175, 370, 170, Color8u(175, 175, 175)); 
-	triangle(pixelArray, 175, 250, 170, Color8u(175, 175, 175));
-	circle(pixelArray, 175, 400, 300, Color8u(100, 100, 100));
-	triangle(pixelArray, 55, 375, 300, Color8u(30, 30, 30));
-	circle(pixelArray, 30, 250, 225, Color8u(70, 255, 70));
-	circle(pixelArray, 40, 550, 225, Color8u(70, 255, 70));
-	rectangle(pixelArray, 495, 520, 400, 450,Color8u(255,0,0));
-	rectangle(pixelArray, 300, 525, 375, 400, Color8u(255,0,0));
+	triangle(pixelArray, 175, 370, 170, Color8u(175, 175, 175)); // right ear
+	triangle(pixelArray, 175, 250, 170, Color8u(175, 175, 175)); // left ear
+	circle(pixelArray, 175, 400, 300, Color8u(100, 100, 100)); // head
+	triangle(pixelArray, 55, 375, 300, Color8u(30, 30, 30)); // nose
+	circle(pixelArray, 30, 250, 225, Color8u(70, 255, 70)); // left eye
+	circle(pixelArray, 40, 550, 225, Color8u(70, 255, 70)); // right eye
+	rectangle(pixelArray, 495, 520, 400, 450,Color8u(255,0,0)); // tounge
+	rectangle(pixelArray, 300, 525, 375, 400, Color8u(255,0,0)); // mouth
 }
 
 void CatPictureApp::draw(){
